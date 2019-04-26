e1a2b39aad7555acb7ee79a0a6377be5ab0ee475
xojan0120 <xojan0120@gmail.com>
2019-04-26 13:53:22 +0900

    [Add] test code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContentLoader from 'react-content-loader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div style={{width:500, height:500}}>
+        <ContentLoader>
+          <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
+          <rect x="25" y="35" rx="5" ry="5" width="220" height="10" />
+          <rect x="25" y="55" rx="5" ry="5" width="220" height="10" />
+          <rect x="25" y="75" rx="5" ry="5" width="220" height="10" />
+        </ContentLoader>
+      </div>
+    );
+  }
 }
 
 export default App;
